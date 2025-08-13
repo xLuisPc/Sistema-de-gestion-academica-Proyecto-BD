d61b77aa388d4b953af56bd24f470c88769c1acb
xLuisPc <lgperez@unillanos.edu.co>
2025-08-13 16:13:40 -0500

    si

---
+++ Admin/config/bd.php
@@ -0,0 +1,4 @@
+<?php
+    $conect = pg_connect("host = suleiman.db.elephantsql.com dbname = ytcjtahq user = ytcjtahq password = 0plXi2Bv51llTEzzGRX1rvkJobro2EZg");
+?>
+
---
+++ Admin/template/Cabecera.php
@@ -0,0 +1,11 @@
+<!doctype html> 
+<html lang="en">
+    <head>
+        <title>Gestor de la Universidad</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/estilo.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./nodos/sweetalert/dist/sweetalert2.min.css">
+    </head>
